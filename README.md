36dedc2735f10a8076b29f1e438d54ead53b1282
Real-Sayya <148602651+Real-Sayya@users.noreply.github.com>
2024-05-19 02:10:56 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# GSv-Better-Bump
-The Betterbump bot from the startup GSv (__This bot is officially no longer part of the startup__)
+# Bump-Bot
+**Discord.js Server Bump Bot (v12) / Server Advertise Bot**
+
+[![widget](https://discord.com/api/guilds/553942677117337600/widget.png?style=banner2)](https://discord.gg/Emk2udJ)
+
+# ⚙️ Setup
+## 🔧 Configuration
+> **[index.js  line 20](https://github.com/DragonCat4012/Bump-Bot/blob/main/index.js#L20)**
+> ```JS
+> const supportGuildId = ''
+> const supportGuildLogChannelId = ''
+> const Bottoken = ''
+> const ownerID = ''
+> ```
+
+## 🤖 Example Setup
+> ```JS
+> const supportGuildId = '553942677117337600'
+> const supportGuildLogChannelId = '753474865104683110'
+> const Bottoken = 'NzA3NTQ4MDadsfghijklmnzY5.NzA3NTQ4MDMopqrstzMjA4NzY5FSDFSDf.sdf21'
+> const ownerID = "586905336850677760"
+> ```
+
+## 🌿 Run
+```node index.js```
+(remember to install all needed dependecies ;3 Use ```npm i``` to install them )
+
+# 🔧 Commands
+|  Name         | Beschreibung                              |
+| ------------- | ----------------------------------------- |
+| bump          | Bumps your Server                         |
+| channel       | Change your server advertisment channel   |
+| color         | Change your bump embed color              |
+| description   | Change your server description            |
+| goodbye       | Change your server goodbye channel        |
+| help          | Shows you all my Commands                 |
+| invite        | Get my Invite link :3                     |
+| prefix        | Change your server prefix                 |
+| preview       | Shows your server bump embed              |
+| welcome       | Change your server welcome channel        |
+| status        | 'Change the bots status        |
+
+## 🍂 Dependencies
+* [discord.js](https://www.npmjs.com/package/discord.js)
+* [parse-ms](https://www.npmjs.com/package/parse-ms)
+* [sequelize](https://www.npmjs.com/package/sequelize)
+* [sqlite3](https://www.npmjs.com/package/sqlite3)
+
+
+## ☘️ Example
+![Activity](examples/status.png)
+
+```
+Logging in...                            index.js:98
+ > 🗸 Server Cache                        database/dbInit.js:50
+ > 🗸 Cached Database Entries             index.js:71
+ >  Logged in as: Tenshi#3639            index.js:118
+.• ΛΚΘRΛ SΤUDΙΘS •.   >>>  bumped!
+```
+
+# 📸 Showcase
+Note: the default prefix is %. You can change this in the [dbInit File](https://github.com/DragonCat4012/Bump-Bot/blob/main/database/dbInit.js)
+| Bump | Setup | Preview|
+| - | - | -|
+|![Bump Message](examples/bump.png)|![Set Your Add](examples/setup.png)|![Check Your Add](examples/preview.png)|
+
+# 📋 Contributions 
+<a href="https://github.com/DragonCat4012/chia/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=DragonCat4012/Bump-Bot" />
+</a>
---
+++ index.js
@@ -0,0 +1,208 @@
+const fs = require("fs");
+const { join } = require("path");
+const Discord = require("discord.js");
+const { Collection, Client, MessageEmbed } = require("discord.js");
+
+const client = new Client();
+const colors = {
+    error: 0xF91A3C,
+    info: 0x303136,
+    success: 0x13EF8D
+}
+const emotes = {
+    false: "❌",
+    true: "✔️",
+    owner: "👑",
+    bot: '🤖',
+    user: '👤'
+}
+
+const supportGuildId = ''
+const supportGuildLogChannelId = ''
+//Specify your bot token
+const Bottoken = ''
+// Insert here your userid if you wnat to use the status command
+const ownerID = ""
+
+
+const rawEmb = () => {
+    return new MessageEmbed()
+        .setColor(colors.info);
+}
+module.exports = { rawEmb }
+
+client.ownerID = ownerID
+client.colors = colors
+client.emotes = emotes
+client.supportGuildId = supportGuildId
+client.supportGuildLogChannelId = supportGuildLogChannelId
+
+if (!Bottoken) throw new Error('Please enter a Bot Token!');
+
+//==================================================================================================================================================
+//Loading Things
+//==================================================================================================================================================
+const { Server, syncDatabase } = require('./database/dbInit');
+var server_cache = new Collection();
+
+Reflect.defineProperty(server_cache, "getGuild", {
+    /**
+     * @param {number} id Guild ID
+     * @returns {Model} new User
+     */
+    value: async function (id) {
+        var guild = server_cache.get(id);
+        if (!guild) guild = await Server.findOne({ where: { key: id } });
+        if (!guild) {
+            guild = await Server.create({ key: id });
+            server_cache.set(id, guild);
+        }
+        return guild;
+    }
+});
+
+Reflect.defineProperty(server_cache, "getChannel", {
+    /**
+     *  @param {number} id Channel ID
+     * @returns {Model} new User
+     */
+    value: async function () {
+        let arr = []
+        var channels = await Server.findAll();
+        channels.forEach(entry => arr.push(entry.channel))
+        return arr;
+    }
+});
+
+//Sync
+const initDatabase = async () => {
+    await syncDatabase();
+
+    try {
+        for (let entr of (await Server.findAll())) {
+            server_cache.set(entr.user_id, entr);
+        }
+        console.log(" > 🗸 Cached Database Entries");
+    } catch (e) {
+        console.log(" > ❌ Error While Caching Database")
+        console.log(e);
+        //process.exit(1);
+    }
+}
+client.database = { server_cache };
+//==================================================================================================================================================
+//Initialize the Commands
+//==================================================================================================================================================
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+    .readdirSync("./commands")
+    .filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+//==================================================================================================================================================
+//Starting the Bot
+//==================================================================================================================================================
+const start = async () => {
+    try {
+        console.log("Logging in...");
+        await client.login(Bottoken).catch(e => {
+            console.log(e.code);
+
+            switch (e.code) {
+                case 'TOKEN_INVALID':
+                    console.error(" > ❌ Invalid Token");
+                    break;
+                case 500:
+                    console.error(" > ❌ Fetch Error");
+                    break;
+                default:
+                    console.error(" > ❌ Unknown Error");
+                    console.error(' > ' + e);
+                    break;
+            }
+            setTimeout(() => { throw e }, 5000);
+        });
+        await initDatabase();
+    } catch (e) {
+        console.log(e);
+    }
+}
+start();
+
+client.on("ready", () => {
+    if (!supportGuildId) throw new Error('Please enter your Support-Guild-ID')
+    if (!supportGuildLogChannelId) throw new Error('Please enter your Support-Guild-Log-Channel-ID')
+    console.log(" >  Logged in as: " + client.user.tag);
+    client.user.setPresence({ activity: { name: "Bump your server", type: 'PLAYING' }, status: 'idle' });
+});
+
+client.on('guildMemberAdd', async member => {
+    let { guild } = member
+    let settings = await client.database.server_cache.getGuild(guild.id)
+    if (!settings.wlc) return
+    let ch = await guild.channels.resolve(settings.wlc)
+    if (!ch) {
+        settings.wlc = undefined
+        return settings.save()
+    }
+    let emb = rawEmb().setTitle('Member Joined').setDescription(`${member} joined **${guild.name}**! Welcome you'r member No. **${guild.memberCount}**`)
+    ch.send(emb).catch(() => { })
+})
+
+client.on('guildCreate', async guild => {
+    let supGuild = await client.guilds.resolve(supportGuildId)
+    let channel = await supGuild.channels.resolve(supportGuildLogChannelId)
+    let emb = rawEmb().setTitle('Server joined').setColor(colors.success).setDescription(guild.name)
+    channel.send(emb).catch(() => { })
+})
+
+client.on('guildMemberRemove', async member => {
+    let { guild } = member
+    let settings = await client.database.server_cache.getGuild(guild.id)
+    if (!settings.gb) return
+    let ch = await guild.channels.resolve(settings.gb)
+    if (!ch) {
+        settings.gb = undefined
+        return settings.save()
+    }
+    let emb = rawEmb().setTitle('Member Leaved').setDescription(`${member} leaved from **${guild.name}** Bye Bye`)
+    ch.send(emb).catch(() => { })
+})
+
+client.on("message", async message => {
+    let emb = rawEmb()
+    if (message.author.bot) return;
+    let settings = await client.database.server_cache.getGuild(message.guild.id)
+    let prefix = settings.prefix;
+
+    if (message.mentions.users.first()) {
+        if (message.mentions.users.first().id == client.user.id) message.channel.send('My prefix is ' + settings.prefix)
+    }
+    if (!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.find(cmd =>
+        cmd.commands.includes(commandName)
+    );
+
+    if (!command) {
+        emb.setDescription(`**I don´t know this command. Use ${prefix}help to see my commands** `)
+        return message.channel.send(emb.setColor(colors.error));
+    }
+
+    if (command.perm) {
+        if (!(message.member.hasPermission(command.perm))) {
+            emb.setDescription("**You are missing following permission:** `" + command.perm + "`")
+            return message.channel.send(emb.setColor(colors.error));
+        }
+    }
+    try {
+        message.client = client
+        command.execute(message, args);
+    } catch (error) { console.log(error) }
+});
